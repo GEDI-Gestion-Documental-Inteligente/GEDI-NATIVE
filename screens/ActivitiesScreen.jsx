617037f54a22f5a847d5ffb8f1b519e64438553b
walterlul <walterjobs44@gmail.com>
2023-08-31 09:51:26 -0300

    estilos activities

--- screens/ActivitiesScreen.jsx
+++ screens/ActivitiesScreen.jsx
@@ -17,41 +17,62 @@ export const ActivitiesScreen = () => {
     dispatch(getMyActivities({ id, ticket }));
   }, []);
 
-
   const renderActivity = ({ item }) => (
     <View style={styles.activityContainer}>
       <Text style={styles.activityType}>{item.entry.activityType}</Text>
       <Text style={styles.activityDate}>{item.entry.postedAt}</Text>
-      {/* You can display more details from the activity here */}
     </View>
   );
 
   return (
     <View style={styles.container}>
-        <DropdownMenu/>
-
-      <FlatList
-        data={activities}
-        renderItem={renderActivity}
-        keyExtractor={(item) => item.entry.id.toString()}
-      />
+      <View style={styles.header}>
+        
+        <Text style={styles.activityText}>Últimos movimientos</Text>
+        <DropdownMenu />
+      </View>
+      <View style={styles.body}>
+        <FlatList
+          data={activities}
+          renderItem={renderActivity}
+          keyExtractor={(item) => item.entry.id.toString()}
+          numColumns={1}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    paddingHorizontal: 20,
     backgroundColor: "#4D6F5F",
-    paddingVertical: 50,
+    margin: 0,
+    padding: 0,
+    width: "100%",
+    height: "100%",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent:"space-between",
+    paddingHorizontal:10,
+    paddingVertical:10,
+  },
+  body: {
+    flex:1,
+    backgroundColor:"#d7d7d7",
+    padding:10,
+    margin:20,
+    borderWidth:1
   },
   activityContainer: {
-    backgroundColor: "#ffffff",
-    padding: 10,
-    marginBottom: 15, // Adjust the margin to create more space between items
-    borderRadius: 5,
-    elevation: 2,
+    flex:1,
+    gap:10,
+    margin:10,
+    minWidth:100,
+    borderBottomWidth:1,
+    borderBottomColor:"#797979"
   },
   activityType: {
     fontSize: 16,
@@ -61,6 +82,10 @@ const styles = StyleSheet.create({
   activityDate: {
     color: "#888888",
   },
- 
+  activityText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
 });
 
